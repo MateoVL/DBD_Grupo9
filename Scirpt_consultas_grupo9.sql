f60efe67ad8357c97d9ec916f9c5ca2c7893edf4
Mateo Valle <mateo.valle@usach.cl>
2024-12-23 09:44:33 -0300

    Consulta 7 Lista

--- Scirpt_consultas_grupo9.sql
+++ Scirpt_consultas_grupo9.sql
@@ -20,3 +20,9 @@ FROM VIDEOJUEGO V
 ORDER BY V.Num_Ventas_Videojuego DESC;
 
 --7. Mostrar lista de deseos de un usuario.
+SELECT V.Nombre_Videojuego
+FROM CONTENIDO_LISTAS as CL, VIDEOJUEGO as V, USUARIO as U, LISTA_DESEOS as L
+WHERE "NombreUsuario" = U.Nombre_Usuario 
+AND U.id_usuario = L.Id_usuario_lista 
+AND L.Id_lista_deseos = CL.Id_Lista 
+AND CL.Id_videojuego = V.Id_Videojuego;
