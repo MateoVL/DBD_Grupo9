85f34db0c1ba461b53f27e2c1df1eca288134df1
Martín <163349035+Martin-Araya-G@users.noreply.github.com>
2025-01-03 23:22:13 -0300

    Update Valoracion_Videojuego.java

--- ProyectoDBD9/src/main/java/com/example/ProyectoDBD9/models/Valoracion_Videojuego.java
+++ ProyectoDBD9/src/main/java/com/example/ProyectoDBD9/models/Valoracion_Videojuego.java
@@ -1,4 +1,33 @@
 package com.example.ProyectoDBD9.models;
 
-public class Valoracion_Videojuego {
+public class ValoracionVideojuego {
+    private int idValoracion;
+    private int idVideojuego;
+
+    // Constructor vacío
+    public ValoracionVideojuego() {}
+
+    // Constructor completo
+    public ValoracionVideojuego(int idValoracion, int idVideojuego) {
+        this.idValoracion = idValoracion;
+        this.idVideojuego = idVideojuego;
+    }
+
+    // Getters
+    public int getIdValoracion() {
+        return idValoracion;
+    }
+
+    public int getIdVideojuego() {
+        return idVideojuego;
+    }
+
+    // Setters
+    public void setIdValoracion(int idValoracion) {
+        this.idValoracion = idValoracion;
+    }
+
+    public void setIdVideojuego(int idVideojuego) {
+        this.idVideojuego = idVideojuego;
+    }
 }
