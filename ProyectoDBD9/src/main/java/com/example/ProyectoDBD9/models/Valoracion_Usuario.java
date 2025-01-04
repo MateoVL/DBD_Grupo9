4d85ae33a7bb8300c867d136ae3d8b7c0e046257
Martín <163349035+Martin-Araya-G@users.noreply.github.com>
2025-01-03 23:22:01 -0300

    Update Valoracion_Usuario.java

--- ProyectoDBD9/src/main/java/com/example/ProyectoDBD9/models/Valoracion_Usuario.java
+++ ProyectoDBD9/src/main/java/com/example/ProyectoDBD9/models/Valoracion_Usuario.java
@@ -1,4 +1,33 @@
 package com.example.ProyectoDBD9.models;
 
-public class Valoracion_Usuario {
+public class ValoracionUsuario {
+    private int idUsuario;
+    private int idValoracion;
+
+    // Constructor vacío
+    public ValoracionUsuario() {}
+
+    // Constructor completo
+    public ValoracionUsuario(int idUsuario, int idValoracion) {
+        this.idUsuario = idUsuario;
+        this.idValoracion = idValoracion;
+    }
+
+    // Getters
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public int getIdValoracion() {
+        return idValoracion;
+    }
+
+    // Setters
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setIdValoracion(int idValoracion) {
+        this.idValoracion = idValoracion;
+    }
 }
