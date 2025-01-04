8e42b633bfb26bfdf237447a1893ab1f71cb2bc6
Martín <163349035+Martin-Araya-G@users.noreply.github.com>
2025-01-03 23:21:43 -0300

    Update Valoracion.java

--- ProyectoDBD9/src/main/java/com/example/ProyectoDBD9/models/Valoracion.java
+++ ProyectoDBD9/src/main/java/com/example/ProyectoDBD9/models/Valoracion.java
@@ -1,4 +1,55 @@
 package com.example.ProyectoDBD9.models;
 
+import java.util.Date;
+
 public class Valoracion {
+    private int idValoracion;
+    private String descripcionValoracion;
+    private int puntuacionValoracion;
+    private Date fechaValoracion;
+
+    // Constructor vacío
+    public Valoracion() {}
+
+    // Constructor completo
+    public Valoracion(int idValoracion, String descripcionValoracion, int puntuacionValoracion, Date fechaValoracion) {
+        this.idValoracion = idValoracion;
+        this.descripcionValoracion = descripcionValoracion;
+        this.puntuacionValoracion = puntuacionValoracion;
+        this.fechaValoracion = fechaValoracion;
+    }
+
+    // Getters
+    public int getIdValoracion() {
+        return idValoracion;
+    }
+
+    public String getDescripcionValoracion() {
+        return descripcionValoracion;
+    }
+
+    public int getPuntuacionValoracion() {
+        return puntuacionValoracion;
+    }
+
+    public Date getFechaValoracion() {
+        return fechaValoracion;
+    }
+
+    // Setters
+    public void setIdValoracion(int idValoracion) {
+        this.idValoracion = idValoracion;
+    }
+
+    public void setDescripcionValoracion(String descripcionValoracion) {
+        this.descripcionValoracion = descripcionValoracion;
+    }
+
+    public void setPuntuacionValoracion(int puntuacionValoracion) {
+        this.puntuacionValoracion = puntuacionValoracion;
+    }
+
+    public void setFechaValoracion(Date fechaValoracion) {
+        this.fechaValoracion = fechaValoracion;
+    }
 }
